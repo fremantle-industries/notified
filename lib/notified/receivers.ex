1f1ee7bef39cc27af5b5a99d1c9ab99011743bc2
Alex <alex+git@fremantle.io>
2021-05-14 23:36:51 -0700

    Send pubsub message when receivers have sent

--- lib/notified/receivers.ex
+++ lib/notified/receivers.ex
@@ -1,4 +1,6 @@
 defmodule Notified.Receivers do
+  alias Notified.{Config, Receiver, Topic}
+
   def send(notification) do
     Notified.Config.receivers()
     |> Enum.map(fn {mod, attrs} ->
@@ -7,6 +9,15 @@ defmodule Notified.Receivers do
       end
     end)
     |> Enum.filter(& &1)
-    |> Enum.each(&Notified.Receiver.send(&1, notification))
+    |> Enum.map(fn receiver ->
+      :ok = Receiver.send(receiver, notification)
+      receiver
+    end)
+    |> Enum.each(fn receiver ->
+      pubsub_server = Config.pubsub_server()
+      msg = {:notified, :receiver_sent, {receiver, notification}}
+      Phoenix.PubSub.broadcast(pubsub_server, Topic.receiver_sent(notification.id), msg)
+      Phoenix.PubSub.broadcast(pubsub_server, Topic.receiver_sent("*"), msg)
+    end)
   end
 end
--- lib/notified/topic.ex
+++ lib/notified/topic.ex
@@ -1,13 +1,16 @@
 defmodule Notified.Topic do
   @spec create(integer | String.t()) :: String.t()
-  def create(id), do: "create:#{id}"
+  def create(id), do: "notified:create:#{id}"
 
   @spec mark_seen(integer | String.t()) :: String.t()
-  def mark_seen(id), do: "mark_seen:#{id}"
+  def mark_seen(id), do: "notified:mark_seen:#{id}"
 
   @spec delete(integer | String.t()) :: String.t()
-  def delete(id), do: "delete:#{id}"
+  def delete(id), do: "notified:delete:#{id}"
 
   @spec clear :: String.t()
-  def clear, do: "clear"
+  def clear, do: "notified:clear"
+
+  @spec receiver_sent(integer | String.t()) :: String.t()
+  def receiver_sent(id), do: "notified:receiver_sent:#{id}"
 end
